69e557db0beea6e271fed43d289ac88187614772
samikshawattamwar <77261236+samikshawattamwar@users.noreply.github.com>
2022-02-06 13:12:34 +0530

    Create SQL Lesson 1: SELECT queries 101.sql

---
+++ SQL Lesson 1: SELECT queries 101.sql
@@ -0,0 +1,19 @@
+-- Find the title of each film
+SELECT Title
+FROM Movies;
+
+-- Find the director of each film
+SELECT Director
+FROM Movies;
+
+-- Find the title and director of each film
+SELECT Title, Director
+FROM Movies;
+
+-- Find the title and year of each film
+SELECT Title, Director
+FROM Movies;
+
+-- Find all the information about each film
+SELECT *
+FROM Movies;
